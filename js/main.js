5bec35934c76e1e64d8494ddfe36ffef47e421ac
HIKKO624 <hikko624@gmail.com>
2019-03-12 13:56:56 +0900

    complete omikuji

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 "use strict";
 {
     const btn = document.getElementById("btn");
-
+    const results = ["大吉", "中吉", "小吉", "末吉", "吉", "凶", "大凶"];
     btn.addEventListener("click", () => {
-        btn.textContent = "hit!";
+        const n = Math.floor(Math.random() * results.length);
+        btn.textContent = results[n];
     });
 
     btn.addEventListener("mousedown", () => {
